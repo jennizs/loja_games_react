0138a77d878830d65648680ba3a1fc4dab6673ae
jennizs <jennyfrn6104@gmail.com>
2024-05-25 18:45:39 -0300

    criando_rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css';
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/login/Login';
+import FormularioCategoria from './components/categorias/formCategoria/FormCategoria';
+import DeleteCategoria from './components/categorias/deleteCategoria/DeleteCategoria';
+import ListarCategoria from './components/categorias/listarCategoria/ListarCategoria';
 
 
 function App() {
@@ -13,8 +17,12 @@ function App() {
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Home />} />
-              {/* <Route path="/login" element={<Login />} /> */}
+               <Route path="/login" element={<Login />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListarCategoria />} />
+              <Route path="/cadastrar-Categoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeleteCategoria />} />
             </Routes>
           </div>
           <Footer />
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,31 +1,45 @@
 import { Basket } from "@phosphor-icons/react"
-import { Link } from 'react-router-dom'
+import { useContext } from "react"
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from "../../contexts/AuthContext"
 
 
 function Navbar() {
- 
-  
+
+
+  const navigate = useNavigate()
+
+  const { handleLogout } = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert('o usuario foi desconectado!')
+    navigate('/login')
+  }
+
+
+
 
   return (
     <>
       <div className='w-full  bg-[#fca5a5] text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>JennizsGames.com</div>
+        <div className="container flex justify-between text-lg">
+          <div className='text-2xl font-bold uppercase'>JennizsGames.com</div>
 
-            <div className='flex gap-4'>
-              <Link to='/login' className='hover:underline'>Login</Link>
-              <Link to='/home' className='hover:underline'>Home</Link>
-              <div className='hover:underline'>Produtos</div>
-              <div className='hover:underline'>Categorias</div>
-              <div className='hover:underline'>Nova Categoria</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='underline-basket'>
-        <Basket size={25} weight='bold' />
-      </div>
-              <div className='hover:underline'>Sair</div>
+          <div className='flex gap-4'>
+            <Link to='/login' className='hover:underline'>Login</Link>
+            <Link to='/home' className='hover:underline'>Home</Link>
+            <div className='hover:underline'>Produtos</div>
+            <div className='hover:underline'>Categorias</div>
+            <div className='hover:underline'>Nova Categoria</div>
+            <div className='hover:underline'>Perfil</div>
+            <div className='underline-basket'>
+              <Basket size={25} weight='bold' />
             </div>
+            <Link to="" onClick={logout} className='hover:underline'>Sair</Link>
           </div>
         </div>
+      </div>
     </>
   )
 }
